4a45786dd73caee30eba4f5f9df39d632bd48781
ryanmbigelow <ryanmbigelow@gmail.com>
2023-06-10 10:37:02 -0500

    completed view artist details ticket

--- tunaapi/models/song.py
+++ tunaapi/models/song.py
@@ -4,6 +4,6 @@ from .artist import Artist
 class Song(models.Model):
 
     title = models.CharField(max_length=50)
-    artist_id = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    artist_id = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
     album = models.CharField(max_length=50)
     length = models.IntegerField()
